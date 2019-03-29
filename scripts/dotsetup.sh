f13ba9125618fd2e817be5f01ae0291dc9c1880a
Glukoosi <aleksi@seppanen.me>
2019-03-29 12:15:23 +0200

    Fix dotsetup.sh and update i3 for manjaro

--- scripts/dotsetup.sh
+++ scripts/dotsetup.sh
@@ -1,5 +1,5 @@
-dir=~/dotfiles
-olddir=~/dotfiles_old
+dir=~/.files
+olddir=~/.files_old
 files=".zshrc .vimrc .i3 .Xresources .ssh/config"
 
 mkdir -p $olddir
@@ -7,6 +7,6 @@ mkdir -p $olddir
 cd $dir
 
 for file in $files; do
-	mv ~/$file ~/dotfiles_old/
+	mv ~/$file $olddir/
 	ln -s $dir/$file ~/$file
 done
